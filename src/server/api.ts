b159c6b979d43eff14ff2a21a6bb106902bed79b
nameless <tistvan1989@gmail.com>
2026-09-14 19:27:19 +0300

    testing the API in Postman

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,4 +1,7 @@
 // src/server/api.ts
 import { remultApi } from 'remult/remult-express'
+import { Task } from '../shared/Task.js'
 
-export const api = remultApi()
+export const api = remultApi({
+    entities: [Task]
+})
---
+++ src/shared/Task.ts
@@ -0,0 +1,20 @@
+// src/shared/Task.ts
+
+import { Entity, Fields } from 'remult'
+
+@Entity('tasks', {
+    allowApiCrud: true
+})
+export class Task {
+    @Fields.id()
+    id = ''
+
+    @Fields.string()
+    title = ''
+
+    @Fields.boolean()
+    completed = false
+
+    @Fields.createdAt()
+    createdAt?: Date
+}
